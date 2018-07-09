88dcf1b4e24c8a815a1e088c3c97cd444fd92437
Micah Shute <micah.shute@gmail.com>
2018-07-09 00:45:17 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,7 +18,7 @@ class Scraper
 
   def get_courses
     doc = get_page
-    data = doc.css("article h2")
+    data = doc.css("article.post.same-height-left.same-height-right h2")
     text = data.map{|title| title.text}
     binding.pry
   end
